99379330b1e00f0afd22fafebda17618b6ffc2b5
sopheak012 <36690234+sopheak012@users.noreply.github.com>
2023-04-27 22:20:23 -0700

    chat room added

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -1,14 +1,43 @@
 import { socket } from "../socket/initSocket";
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import {
+  leaveGame,
+  deleteGame,
+  updateState,
+} from "../features/gameList/gameList";
+import { useDispatch, useSelector } from "react-redux";
 import "../css/ChatBox.css";
 
 const ChatBox = () => {
   const { username, lobbyName } = useParams();
   const [messages, setMessages] = useState([]);
   const [currentMessage, setCurrentMessage] = useState("");
+  const [isHost, setIsHost] = useState(false);
+  const gameList = useSelector((state) => state.gameList.gameList);
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   useEffect(() => {
+    //get initial update
+    socket.emit("get-update", (update) => {
+      dispatch(updateState(update));
+      handleGetUpdate(update);
+    });
+
+    socket.on("update", (update) => {
+      dispatch(updateState(update));
+      handleGetUpdate(update);
+    });
+
+    //check if host leve the lobby
+    socket.on("delete-room", () => {
+      socket.emit("leave-room", lobbyName);
+      navigate(`/user/${username}`);
+    });
+
+    //join the room
     socket.emit("join-room", lobbyName);
 
     socket.on("receive-message", (message) => {
@@ -19,10 +48,20 @@ const ChatBox = () => {
     return () => {
       socket.off("leave-room", lobbyName);
       socket.off("receive-message");
+      socket.off("update");
     };
   }, []);
 
+  const handleGetUpdate = (update) => {
+    const getLobby =
+      update.find((game) => game.lobbyName === lobbyName)?.playerList || [];
+    setIsHost(
+      getLobby.some((player) => player.isHost && player.username === username)
+    );
+  };
+
   const sendMessage = () => {
+    console.log(isHost);
     if (currentMessage.trim() !== "") {
       socket.emit("send-message", lobbyName, {
         username: username,
@@ -30,6 +69,17 @@ const ChatBox = () => {
       });
       setCurrentMessage("");
     }
+  };
+
+  const handleLeave = () => {
+    if (!isHost) {
+      dispatch(leaveGame({ lobbyName, username }));
+      socket.emit("leave-room", lobbyName);
+    } else {
+      dispatch(deleteGame(lobbyName));
+      socket.emit("delete-room", lobbyName);
+    }
+    navigate(`/user/${username}`);
   };
 
   return (
@@ -57,6 +107,7 @@ const ChatBox = () => {
         <button className="chat-send-button" onClick={sendMessage}>
           Send Message
         </button>
+        <button onClick={handleLeave}>Leave</button>
       </div>
     </div>
   );
